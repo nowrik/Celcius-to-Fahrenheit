4becf24809c14c503e51d362230c63d1358b0d1a
Noerick Sentillas <Noerick Sentillas@DESKTOP-79JEEU3>
2021-09-03 16:41:45 +0800

    Add project files.

---
+++ num 5 pg 190/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace num_5_pg_190
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ number 5 pg 190/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace number_5_pg_190
+{
+    class Program
+    {
+        static void Main()
+        {
+            double ftemp;
+
+            Console.Write("Enter Heat Temperature in Fahrenheit: ");
+            ftemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEntered Temperature in Fahrenheit: {0} °F", ftemp);
+            Console.WriteLine("Temperature Value in Celcius: {0:N0} °C", FtoC(ftemp));
+            Console.WriteLine("\nPress Enter Twice to Quit.");
+            Console.Read();
+        }
+        public static double FtoC (double ToCelcius)
+        {
+            return ((ToCelcius - 32) * 5) / 9;
+        }
+    }
+}
